743a5fe037ae4080913438ad16db88ccc95885a3
Balasubramanian G <balagomathinayagam@gmail.com>
2026-03-20 02:04:22 +0530

    change in the db config file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 //local dbconnection
-import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/pet')
-.then(()=>console.log("database is connected"))
-.catch((err)=>console.log('connection is failed:',err))
+// import mongoose from "mongoose";
+// mongoose.connect('mongodb://127.0.0.1:27017/pet')
+// .then(()=>console.log("database is connected"))
+// .catch((err)=>console.log('connection is failed:',err))
 
-export default mongoose;
+// export default mongoose;
 
 
 //Atlas db connection
-/*
+
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
@@ -19,4 +19,4 @@ mongoose.connect(uri)
 .then(()=>console.log("database is connected"))
 .catch((err)=>console.log('connection is failed:',err))
 
-export default mongoose*/
+export default mongoose
