6db5f822cd3f881d764bd7a6200b949b7d803893
Balasubramanian G <balagomathinayagam@gmail.com>
2026-04-05 23:04:48 +0530

    resloved  the favorite page UI changes

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -21,14 +21,17 @@ export default function Favorites() {
       <h2 className="text-2xl font-bold mb-6">Favorites</h2>
       {favs.length === 0 && <p className="text-gray-500 text-center">No favorites yet</p>}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {favs.map((f) => (
-          <div key={f._id} onClick={() => navigate(`/pet/${f.petId._id}`)} className="bg-white rounded-xl shadow-md hover:shadow-lg transition p-4 cursor-pointer">
-            {(<img src={f.petId?.photos?.[0] ? `${BASE_URL}${f.petId.photos[0]}` : "/no-image.png"}alt={f.petId.name}className="w-full h-48 object-cover rounded"/>)}
-            <h3 className="text-lg font-bold mt-3" >{f.petId?.name}</h3>
-            <p className="text-gray-600">{f.petId?.breed}</p>
-            <button onClick={(e) =>{e.stopPropagation();dispatch(removeFav({ userId: user._id, petId: f.petId._id }))}}className="text-red-500 mt-2">Remove</button>
-          </div>
-        ))}
+        {favs.map((f) => {
+          if (!f.petId) return null;
+          return(
+            <div key={f._id} onClick={() => navigate(`/pet/${f.petId._id}`)} className="bg-white rounded-xl shadow-md hover:shadow-lg transition p-4 cursor-pointer">
+              {(<img src={f.petId?.photos?.[0] ? `${BASE_URL}${f.petId.photos[0]}` : "/no-image.png"}alt={f.petId.name || "pet"}className="w-full h-48 object-cover rounded"/>)}
+              <h3 className="text-lg font-bold mt-3" >{f.petId?.name}</h3>
+              <p className="text-gray-600">{f.petId?.breed}</p>
+              <button onClick={(e) =>{e.stopPropagation();dispatch(removeFav({ userId: user?._id, petId: f.petId._id }))}}className="text-red-500 mt-2">Remove</button>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
